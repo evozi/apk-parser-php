c28bebc8fb51811ea38619f67a40737df035c71e
Tufan Baris Yildirim <tufanbarisyildirim@gmail.com>
2017-04-15 02:10:15 +0800

    added magic get/set methods to config.

--- lib/ApkParser/Parser.php
+++ lib/ApkParser/Parser.php
@@ -5,7 +5,6 @@ namespace ApkParser;
  * This file is part of the Apk Parser package.
  *
  * (c) Tufan Baris Yildirim <tufanbarisyildirim@gmail.com>
- * (c) Evozi <email@evozi.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -15,6 +14,9 @@ class Parser
 {
     private $apk;
     private $manifest;
+    /**
+     * @var ResourcesParser|null
+     */
     private $resources;
     private $config;
 
@@ -24,13 +26,15 @@ class Parser
      */
     public function __construct($apkFile, array $config = array())
     {
+        $this->config = new Config($config);
         $this->apk = new Archive($apkFile);
         $this->manifest = new Manifest(new XmlParser($this->apk->getManifestStream()));
-        if (empty($config["manifest_only"]))
+
+        if (!$this->config->manifest_only)
             $this->resources = new ResourcesParser($this->apk->getResourcesStream());
         else
             $this->resources = NULL;
-        $this->config = new Config($config);
+
     }
 
     /**
@@ -83,7 +87,7 @@ class Parser
         $dexStream = $this->apk->getClassesDexStream();
         $apkName = $this->apk->getApkName();
 
-        $cache_folder = $this->config->get('tmp_path') . '/' . str_replace('.', '_', $apkName) . '/';
+        $cache_folder = $this->config->tmp_path . '/' . str_replace('.', '_', $apkName) . '/';
 
         // No folder means no cached data.
         if (!is_dir($cache_folder))
@@ -94,14 +98,15 @@ class Parser
 
         // run shell command to extract  dalvik compiled codes to the cache folder.
         // Template : java -jar dedexer.jar -d {destination_folder} {source_dex_file}
-        $command = "java -jar {$this->config->get('jar_path')} -d {$cache_folder} {$dex_file}";
+        $command = "java -jar {$this->config->jar_path} -d {$cache_folder} {$dex_file}";
         $returns = shell_exec($command);
 
         if (!$returns) //TODO : check if it not contains any error. $returns will always contain some output.
             throw new \Exception("Couldn't decompile .dex file");
 
-        $file_list = \ApkParser\Utils::globRecursive($cache_folder . '*.ddx');
+        $file_list = Utils::globRecursive($cache_folder . '*.ddx');
 
+        //Make classnames more readable.
         foreach ($file_list as &$file) {
             $file = str_replace($cache_folder, '', $file);
             $file = str_replace('/', '.', $file);
@@ -109,6 +114,8 @@ class Parser
             $file = trim($file, '.');
         }
 
+
         return $file_list;
+
     }
 }
