5ccff4fd76aac6851cb8a93ebcdc8220fb79b097
Burhan Ibrahimi <burhan@wflux.pro>
2021-12-19 02:23:53 +0100

    Fix issue  with PHP 8.0
    This is changed sing PHP 7.4

--- lib/ApkParser/XmlParser.php
+++ lib/ApkParser/XmlParser.php
@@ -399,7 +399,7 @@ class XmlParser
         }
         $length = strlen($value);
         for ($i = 0; $i < $length; $i++) {
-            $current = ord($value{$i});
+            $current = ord($value[$i]);
             if (($current == 0x9) ||
                 ($current == 0xA) ||
                 ($current == 0xD) ||
